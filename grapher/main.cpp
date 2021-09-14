764d46d51d81b300f939273d2bbaa6e2ac99c5c3
gubanovpm <70346831+gubanovpm@users.noreply.github.com>
2021-09-14 13:52:44 +0300

    Update main.cpp

--- grapher/main.cpp
+++ grapher/main.cpp
@@ -1,4 +1,6 @@
 #include "graphics.hpp"
+#include <thread>
+#include <chrono>
 
 int main() {
 	
@@ -12,7 +14,6 @@ int main() {
 	Vector_t firstAbscissa (0.05 * scrX, 0.85 * scrY, 0.4 * scrX, 0);
 	Plot_t firstPlot(&firstAbscissa, &firstOrdinate,  0, 0, firstPlotColor);
 
-
 	Vector_t secondOrdinate (0.55 * scrX, 0.85 * scrY, 0, -0.8 * scrY);
 	Vector_t secondAbscissa (0.55 * scrX, 0.85 * scrY, 0.4 * scrX, 0);
 	Plot_t secondPlot(&secondAbscissa, &secondOrdinate, 0, 0, firstPlotColor);
@@ -20,6 +21,8 @@ int main() {
 	Vector_t a(0.55 * scrX, 0.85 * scrY, 100, -50);
 	Vector_t b(0.55 * scrX, 0.85 * scrY, 50, -100);
 	Vector_t c = a + b;
+
+	Vector_t r(0.5 * scrX , 0.5 * scrY, 0, 200);
 	
 	while (window.isOpen()) {
         sf::Event event;
@@ -33,6 +36,16 @@ int main() {
 		DrawVector(&a, &window, 0xFFFFFF, 0.25);
 		DrawVector(&b, &window, 0xFFFFFF, 0.25);
 		DrawVector(&c, &window, 0xFFFFFF, 0.25);
+
+		double angle = M_PI / 50, temp_angle = M_PI / 50;
+		while (angle < 2 * M_PI) {
+			DrawVector(&r, &window, 0xFFFFFF, 0.25);
+			r.RightRotate(temp_angle);
+			angle += temp_angle;
+		}
+
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
+
 		void DrawVector(Vector_t *vector, sf::RenderWindow *window,int color, double fraction);
 		firstPlot.Draw(&window);
 		secondPlot.Draw(&window);
